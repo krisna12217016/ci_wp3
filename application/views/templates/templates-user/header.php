758a64953132ada69dbce22915ae41da7c3fb3e2
Krisna12217016 <12217016@bsi.ac.id>
2022-03-26 22:19:28 +0700

    menambahkan file footer serta revisi file header

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pustaka-Booking | <?= $judul; ?></title>
     <link rel="icon" type="image/png" href="<?= base_url('assets/img/logo/'); ?>logo-pb.png">
-    <link rel="sylesheet" href="<?= base_url('assets/'); ?>user/css/bootstrap.css">
+    <link rel="sylesheet" href="<?= base_url('assets/'); ?>assets/user/css/bootstrap.css">
     <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="<?= base_url('assets/'); ?>datatable/datatables.css" rel="stylesheet" type="text/css">
 </head>
@@ -28,4 +28,10 @@
                         <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
                         <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log Out</a>
                     <?php } ?>
+                    <span class="nav-item nav-link nav-right" style="display:block; margin-left:20px;">Selamat Datang <b><?= $user; ?></br></span>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-5">
 </body>
